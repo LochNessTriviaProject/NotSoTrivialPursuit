aacf186583c1f0b337d92dad215da8b9ef7c76d3
Seunggi Lee <seunggi.lee21@gmail.com>
2021-05-20 16:18:43 -0400

    initial folder setup

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import '../styles/App.css';
+import firebase from "../config/firebase";
+
+function App() {
+
+
+
+
+
+
+
+
+  
+  return (
+    <>
+
+
+
+
+
+
+
+    </>
+
+  );
+}
+
+export default App;
