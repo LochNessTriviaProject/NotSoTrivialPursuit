eeb05dd0c4da7a3a0d5ec97d4b0aefcc96695f40
Seunggi Lee <seunggi.lee21@gmail.com>
2021-05-27 18:02:31 -0400

    code cleanup for production

--- src/components/App.js
+++ src/components/App.js
@@ -13,20 +13,18 @@ import spiral from "../assets/spiral.png";
 import Header from "./Header";
 
 function App() {
-
   const [displayTrivia, setDisplayTrivia] = useState(false);
   const [quizAmount, setQuizAmount] = useState(10);
-  const [quizCategory, setQuizCategory] = useState(14); // 9(general knowledge) ~ 32(entertainment)
+  const [quizCategory, setQuizCategory] = useState(14);
   const [quizDifficulty, setQuizDifficulty] = useState("hard");
   const quizType = "multiple";
   const [savedGame, setSavedGame] = useState(false);
   const [savedQuizArray, setSavedQuizArray] = useState([1, 2, 3]);
-  const [quizArray, setQuizArray] = useState([1,2,3]);
+  const [quizArray, setQuizArray] = useState([1, 2, 3]);
   const [quizCount, setQuizCount] = useState(0);
   const [quizScore, setQuizScore] = useState(0);
   const [userData, setUserData] = useState([]);
   const dbRef = firebase.database().ref();
-  // test change
 
   useEffect(() => {
     dbRef.on("value", (res) => {
@@ -46,12 +44,10 @@ function App() {
   }, []);
 
   const unicodeReplacer = (string) => {
-
     return string
       .replace(/&quot;/g, `"`)
       .replace(/&#039;/g, `'`)
       .replace(/&ouml;/g, "ö");
-
   };
 
   const handleCategory = (event) => {
@@ -84,7 +80,6 @@ function App() {
         const counter = Object.keys(data[key]).length;
 
         for (let i = 0; i < counter - 3; i++) {
-
           let searchObj = {
             key: key,
             name: data[key][i].name,
@@ -93,87 +88,84 @@ function App() {
             wrongAnswer1: data[key][i].wrongAnswer1,
             wrongAnswer2: data[key][i].wrongAnswer2,
             wrongAnswer3: data[key][i].wrongAnswer3,
-            quizLength: data[key][i].quizLength
+            quizLength: data[key][i].quizLength,
           };
-
 
           newDataArray.push(searchObj);
         }
       }
-      
+
       updatedArray = newDataArray.filter((user) => {
         return user.name === userNameValue;
       });
     });
 
     if (updatedArray[0]) {
-      alert('cant use same name twice')
-    }
-    else{
-    axios({
-      method: "GET",
-      url: "https://opentdb.com/api.php",
-      responseType: "json",
-      params: {
-        amount: quizAmount,
-        category: quizCategory,
-        difficulty: quizDifficulty,
-        type: quizType,
-      },
-    }).then((res) => {
-      if (res.data.response_code === 0) {
-        // quotes and '(apostrophe) are turning into weird unicodes (&#039;) from API call right away, there should be way to convert/fix this?
+      Swal.fire({
+        title: "Uh oh",
+        text: "Cannot have duplicate user names! (no authentication!) Please choose another user name again!",
+        imageUrl:
+          "https://images.blush.design/UMepYocbuMn1l5E92vl7?w=920&auto=compress&cs=srgb",
+        imageWidth: 250,
+        imageAlt: "Custom image",
+      });
+    } else {
+      axios({
+        method: "GET",
+        url: "https://opentdb.com/api.php",
+        responseType: "json",
+        params: {
+          amount: quizAmount,
+          category: quizCategory,
+          difficulty: quizDifficulty,
+          type: quizType,
+        },
+      }).then((res) => {
+        if (res.data.response_code === 0) {
+          const quizObjArray = res.data.results;
+          console.log(quizObjArray);
 
-        const quizObjArray = res.data.results;
-        console.log(quizObjArray);
-
-        const newQuizArray = quizObjArray.map((quiz, index) => {
-
-          return {
-            name: userNameValue,
-            quizLength: quizObjArray.length,
-            category: quizObjArray[index].category,
-            key: `quiz-${index}`,
-            question: unicodeReplacer(quizObjArray[index].question),
-            correctAnswer: unicodeReplacer(quizObjArray[index].correct_answer),
-            wrongAnswer1: unicodeReplacer(
-              quizObjArray[index].incorrect_answers[0]
-            ),
-            wrongAnswer2: unicodeReplacer(
-              quizObjArray[index].incorrect_answers[1]
-            ),
-            wrongAnswer3: unicodeReplacer(
-              quizObjArray[index].incorrect_answers[2]
-            ),
-          };
-        });
-        newQuizArray.progress = 0;
-        newQuizArray.score = 0;
-        newQuizArray.completed = false;
-        console.log(newQuizArray);
-        setQuizArray(newQuizArray);
-        dbRef.push(newQuizArray);
-        setDisplayTrivia(true);
-        
-      } else {
-        Swal.fire({
-          title: "Uh oh!",
-          text: "We don't have enough questions in that category to challenge you.",
-          imageUrl:
-            "https://images.blush.design/UMepYocbuMn1l5E92vl7?w=920&auto=compress&cs=srgb",
-          imageWidth: 300,
-          imageAlt: "Custom image",
-        });
-      }
-    });
+          const newQuizArray = quizObjArray.map((quiz, index) => {
+            return {
+              name: userNameValue,
+              quizLength: quizObjArray.length,
+              category: quizObjArray[index].category,
+              key: `quiz-${index}`,
+              question: unicodeReplacer(quizObjArray[index].question),
+              correctAnswer: unicodeReplacer(
+                quizObjArray[index].correct_answer
+              ),
+              wrongAnswer1: unicodeReplacer(
+                quizObjArray[index].incorrect_answers[0]
+              ),
+              wrongAnswer2: unicodeReplacer(
+                quizObjArray[index].incorrect_answers[1]
+              ),
+              wrongAnswer3: unicodeReplacer(
+                quizObjArray[index].incorrect_answers[2]
+              ),
+            };
+          });
+          newQuizArray.progress = 0;
+          newQuizArray.score = 0;
+          newQuizArray.completed = false;
+          console.log(newQuizArray);
+          setQuizArray(newQuizArray);
+          dbRef.push(newQuizArray);
+          setDisplayTrivia(true);
+        } else {
+          Swal.fire({
+            title: "Uh oh!",
+            text: "We don't have enough questions in that category to challenge you.",
+            imageUrl:
+              "https://images.blush.design/UMepYocbuMn1l5E92vl7?w=920&auto=compress&cs=srgb",
+            imageWidth: 250,
+            imageAlt: "Custom image",
+          });
+        }
+      });
     }
   };
-
-  // const handleUserName = (event) => {
-  //   let userNameValue = event.target.value;
-  //   setUserName(userNameValue);
-  //   console.log(userNameValue);
-  // };
 
   const handleAnswerChoice = (event, quizLength, userSavedName) => {
     const buttonClassName = event.target.className;
@@ -181,28 +173,22 @@ function App() {
 
     if (quizCount === quizLength - 1) {
       if (buttonClassName === "correct") {
-        //SHOW CORRECT ANIMATION HERE?
         setQuizScore(quizScore + 1);
-
         endGame(userSavedName);
       } else {
-        //SHOW INCORRECT ANIMATION HERE?
         endGame(userSavedName);
       }
     } else {
       if (buttonClassName === "correct") {
-        //SHOW CORRECT ANIMATION HERE?
         setQuizScore(quizScore + 1);
         setQuizCount(quizCount + 1);
       } else {
-        //SHOW INCORRECT ANIMATION HERE?
         setQuizCount(quizCount + 1);
       }
     }
   };
 
   const endGame = (savedUserName) => {
-    // when user wants to end game, hide modal
     console.log(savedUserName);
     let updatedArray = [];
     dbRef.on("value", (res) => {
@@ -213,7 +199,6 @@ function App() {
         const counter = Object.keys(data[key]).length;
 
         for (let i = 0; i < counter - 3; i++) {
-
           let searchObj = {
             key: key,
             name: data[key][i].name,
@@ -222,9 +207,8 @@ function App() {
             wrongAnswer1: data[key][i].wrongAnswer1,
             wrongAnswer2: data[key][i].wrongAnswer2,
             wrongAnswer3: data[key][i].wrongAnswer3,
-            quizLength: data[key][i].quizLength
+            quizLength: data[key][i].quizLength,
           };
-
 
           newDataArray.push(searchObj);
         }
@@ -236,15 +220,20 @@ function App() {
     });
     const savedDbRef = firebase.database().ref(updatedArray[0].key);
 
-
-    if (quizCount === updatedArray[0].quizLength-1) {
-
-      // savedDbRef.remove();
-      // setQuizArray([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
-
-      savedDbRef.update({completed:true, progress:quizCount+1, score: quizScore});
-      alert("yay u finished quiz!");
-
+    if (quizCount === updatedArray[0].quizLength - 1) {
+      savedDbRef.update({
+        completed: true,
+        progress: quizCount + 1,
+        score: quizScore,
+      });
+      Swal.fire({
+        title: "YAY",
+        text: "You completed the quiz!",
+        imageUrl:
+          "https://images.blush.design/UMepYocbuMn1l5E92vl7?w=920&auto=compress&cs=srgb",
+        imageWidth: 250,
+        imageAlt: "Custom image",
+      });
     } else {
       const data = {
         progress: quizCount,
@@ -259,7 +248,6 @@ function App() {
   };
 
   const resumeGame = (savedUserKey) => {
-
     dbRef.on("value", (res) => {
       const newDataArray = [];
       const data = res.val();
@@ -285,7 +273,6 @@ function App() {
       console.log("newdataArray", newDataArray);
       console.log(savedUserKey);
 
-
       const updatedArray = newDataArray.filter((user) => {
         return user.key === savedUserKey;
       });
@@ -294,7 +281,7 @@ function App() {
 
       setSavedQuizArray(updatedArray);
 
-      console.log(updatedArray)
+      console.log(updatedArray);
       setQuizCount(updatedArray[0].progress);
       setQuizScore(updatedArray[0].score);
       setSavedGame(true);
@@ -303,10 +290,9 @@ function App() {
     setDisplayTrivia(true);
   };
 
-  
   return (
     <>
-      <Header/>
+      <Header />
 
       <main>
         <div className="wrapper">
@@ -318,7 +304,6 @@ function App() {
                 handleAnswerChoice={handleAnswerChoice}
                 quizScore={quizScore}
                 endGame={endGame}
-                //if else statement to show saved games instead of fresh api called games!
               />
             ) : (
               <div aria-hidden="true"></div>
@@ -330,7 +315,6 @@ function App() {
               handleAnswerChoice={handleAnswerChoice}
               quizScore={quizScore}
               endGame={endGame}
-              //if else statement to show saved games instead of fresh api called games!
             />
           ) : (
             <div aria-hidden="true"></div>
@@ -347,13 +331,42 @@ function App() {
         </div>
 
         <img src={shapes} alt="" className="shapes" aria-hidden="true" />
-        <img src={spiral} alt="" className="spiral spiralOne" aria-hidden="true" />
-        <img src={spiral} alt="" className="spiral spiralTwo" aria-hidden="true" />
-        <img src={spiral} alt="" className="spiral spiralThree" aria-hidden="true" />
-        <img src={spiral} alt="" className="spiral spiralFour" aria-hidden="true" />
-        <img src={spiral} alt="" className="spiral spiralFive" aria-hidden="true" />
-        <img src={spiral} alt="" className="spiral spiralSix" aria-hidden="true" />
-
+        <img
+          src={spiral}
+          alt=""
+          className="spiral spiralOne"
+          aria-hidden="true"
+        />
+        <img
+          src={spiral}
+          alt=""
+          className="spiral spiralTwo"
+          aria-hidden="true"
+        />
+        <img
+          src={spiral}
+          alt=""
+          className="spiral spiralThree"
+          aria-hidden="true"
+        />
+        <img
+          src={spiral}
+          alt=""
+          className="spiral spiralFour"
+          aria-hidden="true"
+        />
+        <img
+          src={spiral}
+          alt=""
+          className="spiral spiralFive"
+          aria-hidden="true"
+        />
+        <img
+          src={spiral}
+          alt=""
+          className="spiral spiralSix"
+          aria-hidden="true"
+        />
       </main>
 
       <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
-
-
 const Header = () => {
-    return(
-        <header>
-        <div className="wrapper">
-          <h1>
-            <span className="notSo">Not So</span>{" "}
-            <span className="trivial">Trivial</span>{" "}
-            <span className="pursuit">Pursuit</span>
-          </h1>
-        </div>
-      </header>
-    )
-}
+  return (
+    <header>
+      <div className="wrapper">
+        <h1>
+          <span className="notSo">Not So</span>{" "}
+          <span className="trivial">Trivial</span>{" "}
+          <span className="pursuit">Pursuit</span>
+        </h1>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
